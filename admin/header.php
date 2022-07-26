def1a7198b301c976e21f9797543faab910e8522
Prince Kushwaha <princekushwaha22@yahoo.com>
2022-07-26 08:52:51 +0545

    working on admindashboard

---
+++ admin/header.php
@@ -0,0 +1,7 @@
+
+<html>
+<head>
+<title>jobKhojam: Admin Dashboard</title>
+<link href="css/admin-style.css" rel="stylesheet" type="text/css"> 
+</head>
+<body>
---
+++ admin/headtitle.php
@@ -0,0 +1,5 @@
+<div class="admindashtitle">
+		<a href="admindash.php" style="float:left;margin-left:30px;font-size:20px; color:#fff;" >Back to Dashboard</a>
+		<a href="../logout.php" style="float:right;margin-right:30px;font-size:20px; color:#fff;" >Logout</a>
+		<h1 align="center">Welcome to Admin Dashboard</h1>
+		</div>
