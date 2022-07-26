eee588bf70223bf580234c492450fb1d9652adf2
Prince Kushwaha <princekushwaha22@yahoo.com>
2022-07-26 09:44:14 +0545

    working with admindashboard

--- admin/headtitle.php
+++ admin/headtitle.php
@@ -20,7 +20,17 @@ else
 <div class="main_wrapper">
 	<div class="admin_container">
 		<div class="right__container">
-			welcome
+			<div class="logo">
+				<a href="/admindash.php"><img src="../assets/images/jp-logo-light.png" alt="jobkhojam_logo"></a>
+			</div>
+		<?php 
+			if(!empty($_SESSION['useremail'])){
+				echo "no user";
+			} else{
+				$a_useremail=$_SESSION['useremail'];
+				echo  $_useremail;
+			}
+		?>
 		</div>
 		<div class="left__container">
 
